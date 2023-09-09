8b9bf2bb02b4f2dbffc4439f32fe5eb4ac79855e
Tzabito <miguel_anxel@hotmail.com>
2023-09-09 15:51:52 -0500

    Update Schedule.vue with i18n

--- studymentor/src/main.js
+++ studymentor/src/main.js
@@ -8,7 +8,7 @@ const i18n =  createI18n({
     locale:'es',
     messages:{
         es:{
-            'registerS':'Registro del estudiante',
+            'registerS':'Registro del Estudiante',
             'registerT':'Registro del Tutor',
             'Schedule':'Horario',
             'Monday':'Lunes',
